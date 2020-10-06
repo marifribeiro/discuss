d382f0b38bad3c21773b7ca81c8a5ac95ed0ea14
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-10-06 10:45:01 -0300

    Finished topics session

--- web/router.ex
+++ web/router.ex
@@ -14,13 +14,8 @@ defmodule Discuss.Router do
   end
 
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    pipe_through :browser
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
